53a57c5933481f0b7ed0b1b3302ff7a1632c10ea
ricardopesce <62214623+ricardopesce@users.noreply.github.com>
2021-01-16 00:14:03 -0300

    improving CategoryList with BaseResourceListComponent

--- src/app/shared/components/base-resource-list/base-resource-list.component.ts
+++ src/app/shared/components/base-resource-list/base-resource-list.component.ts
@@ -6,13 +6,13 @@ import { BaseResourceService } from '../../services/base-resource.service';
 
 export abstract class BaseResourceListComponent<T extends BaseResourceModel> implements OnInit {
 
-	resource: T[] = [];
+	resources: T[] = [];
 
   constructor(private resourceService: BaseResourceService<T>) { }
 
   ngOnInit() {
   	this.resourceService.getAll().subscribe(
-  		resource => this.resource = resource.sort((a, b) => b.id - a.id),
+  		resources => this.resources = resources.sort((a, b) => b.id - a.id),
   		error => alert('Erro ao carregar a lista')
   	)
   }
